f2861038ea4dd7cf182e796e185d6d8e078292ab
HRRSH471 <Hariomharish50@gmail.com>
2026-07-20 09:56:08 +0530

    New Deployment File

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -9,7 +9,7 @@ load_dotenv('/home/jenkins/.env')
 def create_database():
     connection = pymysql.connect(
         host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
+        user=os.getenv("DB_USER")
         password=os.getenv("DB_PASSWORD"),
         port=int(os.getenv("DB_PORT", 3306))
     )
